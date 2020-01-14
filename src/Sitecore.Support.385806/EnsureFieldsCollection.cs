977280e33bcc8c87e2c6d9c1f2494022c99a69b7
Ivan Sheyenko <ivs@sitecore.net>
2020-01-14 13:34:51 -0500

    patch

---
+++ src/Sitecore.Support.385806/EnsureFieldsCollection.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.WFFM.Abstractions.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Forms.Core.Pipelines
+{
+    public class EnsureFieldsCollection
+    {
+        public void Process(ProcessMessageArgs args)
+        {
+            if (args.Fields == null)
+            {
+                args.Fields = new WFFM.Abstractions.Actions.AdaptedResultList(new WFFM.Abstractions.Actions.AdaptedControlResult[0]);
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.385806/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.385806")]
+[assembly: AssemblyProduct("Sitecore.Support.385806")]
+[assembly: ComVisible(false)]
